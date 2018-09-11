c2acf902bc3d046452e76a90ab99bbfe0c293649
camisLuisa <clfl@ecomp.poli.br>
2018-09-11 18:05:47 -0300

    LPS-85317 Liferay Configuration and Login Messages

--- app/src/main/java/mobile/liferay/com/formsscreenletdemo/LoginActivity.java
+++ app/src/main/java/mobile/liferay/com/formsscreenletdemo/LoginActivity.java
@@ -2,6 +2,8 @@ package mobile.liferay.com.formsscreenletdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 import com.liferay.mobile.screens.auth.login.LoginListener;
 import com.liferay.mobile.screens.auth.login.LoginScreenlet;
 import com.liferay.mobile.screens.context.User;
@@ -20,12 +22,14 @@ public class LoginActivity extends AppCompatActivity implements LoginListener {
 
     @Override
     public void onLoginSuccess(User user) {
+        Toast.makeText(LoginActivity.this, "Login is Successful!", Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     public void onLoginFailure(Exception e) {
-
+        Toast.makeText(LoginActivity.this, "Login Faild!", Toast.LENGTH_SHORT).show();
+        Log.e("Test", e.getMessage());
     }
 
     @Override
