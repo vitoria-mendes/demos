2738505ae892706ea696f4cc88733e89fa89ca90
camisLuisa <clfl@ecomp.poli.br>
2018-09-11 16:46:02 -0300

    LPS-85317 Create Login Activity

---
+++ app/src/main/java/mobile/liferay/com/formsscreenletdemo/LoginActivity.java
@@ -0,0 +1,35 @@
+package mobile.liferay.com.formsscreenletdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.liferay.mobile.screens.auth.login.LoginListener;
+import com.liferay.mobile.screens.auth.login.LoginScreenlet;
+import com.liferay.mobile.screens.context.User;
+
+public class LoginActivity extends AppCompatActivity implements LoginListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+
+        LoginScreenlet loginScreenlet = findViewById(R.id.login);
+        loginScreenlet.setListener(this);
+    }
+
+    @Override
+    public void onLoginSuccess(User user) {
+
+    }
+
+    @Override
+    public void onLoginFailure(Exception e) {
+
+    }
+
+    @Override
+    public void onAuthenticationBrowserShown() {
+
+    }
+}
