5ae0650dbbcb8de4714ade879b5b94953f63aab1
camisLuisa <clfl@ecomp.poli.br>
2018-09-18 18:36:22 -0300

    LPS-85527 Include Notification When Login Fail

--- android/forms-screenlet-demo/app/src/main/java/mobile/liferay/com/formsscreenletdemo/LoginActivity.java
+++ android/forms-screenlet-demo/app/src/main/java/mobile/liferay/com/formsscreenletdemo/LoginActivity.java
@@ -1,13 +1,19 @@
 package mobile.liferay.com.formsscreenletdemo;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import com.liferay.mobile.screens.auth.login.LoginListener;
 import com.liferay.mobile.screens.auth.login.LoginScreenlet;
 import com.liferay.mobile.screens.context.User;
+import com.liferay.mobile.screens.util.AndroidUtil;
 
 public class LoginActivity extends AppCompatActivity implements LoginListener {
+	CoordinatorLayout coordinatorLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,7 @@ public class LoginActivity extends AppCompatActivity implements LoginListener {
 
 		setContentView(R.layout.activity_login);
 
+		coordinatorLayout = findViewById(R.id.coordinator);
 		LoginScreenlet loginScreenlet = findViewById(R.id.login);
 		loginScreenlet.setListener(this);
 	}
@@ -27,7 +34,14 @@ public class LoginActivity extends AppCompatActivity implements LoginListener {
 
 	@Override
 	public void onLoginFailure(Exception e) {
-		//TODO: Implement Failure UI
+		int icon = R.drawable.default_error_icon;
+		int backgroundColor =
+			ContextCompat.getColor(this, com.liferay.mobile.screens.viewsets.lexicon.R.color.lightRed);
+		int textColor = Color.WHITE;
+		String message = getString(R.string.loading_failed);
+
+		AndroidUtil.showCustomSnackbar(coordinatorLayout, message, Snackbar.LENGTH_LONG, backgroundColor, textColor,
+			icon);
 	}
 
 	@Override
