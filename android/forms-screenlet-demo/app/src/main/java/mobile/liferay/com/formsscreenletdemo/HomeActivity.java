717d29f5b079fb2113760f467acf23ef29cff0ed
camisLuisa <clfl@ecomp.poli.br>
2018-09-24 18:56:57 -0300

    LPS-85521 Implement car insurance Home Screen

--- android/forms-screenlet-demo/app/src/main/java/mobile/liferay/com/formsscreenletdemo/HomeActivity.java
+++ android/forms-screenlet-demo/app/src/main/java/mobile/liferay/com/formsscreenletdemo/HomeActivity.java
@@ -1,10 +1,18 @@
 package mobile.liferay.com.formsscreenletdemo;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Luísa Lima
@@ -19,6 +27,10 @@ public class HomeActivity extends AppCompatActivity
 
         Button formButton = findViewById(R.id.forms_button);
         formButton.setOnClickListener(this);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.login_status_bar_color));
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
     }
 
     @Override
@@ -26,4 +38,6 @@ public class HomeActivity extends AppCompatActivity
         Intent intent = new Intent(this, FormsActivity.class);
         startActivity(intent);
     }
+
+
 }
