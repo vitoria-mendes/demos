750c4ce5600f0bc00928117e69a68ad77972c28e
camisLuisa <clfl@ecomp.poli.br>
2018-09-24 19:04:12 -0300

    LPS-85521 SF

--- android/forms-screenlet-demo/app/src/main/java/mobile/liferay/com/formsscreenletdemo/HomeActivity.java
+++ android/forms-screenlet-demo/app/src/main/java/mobile/liferay/com/formsscreenletdemo/HomeActivity.java
@@ -4,15 +4,8 @@ import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Luísa Lima
@@ -37,7 +30,7 @@ public class HomeActivity extends AppCompatActivity
     public void onClick(View v) {
         Intent intent = new Intent(this, FormsActivity.class);
         startActivity(intent);
+        
     }
-
 
 }
