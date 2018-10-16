f12d0885a7f1b083b196db0e9901f625cd9d1fb0
camisLuisa <clfl@ecomp.poli.br>
2018-10-16 14:54:36 -0300

    LPS-86217 Include Snackbar and the Correct Dimensions

--- android/forms-screenlet-demo/app/src/main/java/mobile/liferay/com/formsscreenletdemo/ForgotPasswordActivity.java
+++ android/forms-screenlet-demo/app/src/main/java/mobile/liferay/com/formsscreenletdemo/ForgotPasswordActivity.java
@@ -1,28 +1,52 @@
 package mobile.liferay.com.formsscreenletdemo;
 
+import android.graphics.Color;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.liferay.mobile.screens.auth.forgotpassword.ForgotPasswordListener;
 import com.liferay.mobile.screens.auth.forgotpassword.ForgotPasswordScreenlet;
+import com.liferay.mobile.screens.util.AndroidUtil;
 
 public class ForgotPasswordActivity extends AppCompatActivity implements ForgotPasswordListener {
+
+	private CoordinatorLayout coordinatorLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_forgot_password);
 
-		ForgotPasswordScreenlet screenlet = findViewById(R.id.forgot_password_screenlet);
-		screenlet.setListener(this);
+		coordinatorLayout = findViewById(R.id.coordinator_forgot_password);
+
+		ForgotPasswordScreenlet forgotPasswordScreenlet = findViewById(R.id.forgot_password_screenlet);
+		forgotPasswordScreenlet.setAnonymousApiPassword(getString(R.string.liferay_anonymousApiPassword));
+		forgotPasswordScreenlet.setAnonymousApiUserName(getString(R.string.liferay_anonymousApiUserName));
+		forgotPasswordScreenlet.setListener(this);
 	}
 
 	@Override
 	public void onForgotPasswordRequestSuccess(boolean passwordSent) {
+		int backgroundColor =
+			ContextCompat.getColor(this, com.liferay.mobile.screens.viewsets.lexicon.R.color.green_default);
+		int textColor = Color.WHITE;
+		String message = getString(R.string.request_completed);
 
+		AndroidUtil.showCustomSnackbar(coordinatorLayout, message, Snackbar.LENGTH_LONG, backgroundColor, textColor);
 	}
 
 	@Override
 	public void onForgotPasswordRequestFailure(Exception e) {
+		int icon = R.drawable.default_error_icon;
+		int backgroundColor =
+			ContextCompat.getColor(this, com.liferay.mobile.screens.viewsets.lexicon.R.color.lightRed);
+		int textColor = Color.WHITE;
+		String message = getString(R.string.request_failed);
 
+		AndroidUtil.showCustomSnackbar(coordinatorLayout, message, Snackbar.LENGTH_LONG, backgroundColor, textColor,
+			icon);
 	}
+
 }
